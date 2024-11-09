12104f79906be11a1400da0f3b6f5e0ed087c7b0
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-09 22:08:00 +0800

    MariaDB

---
+++ 24.2.Dalmatian/i000.servers/i000.11.Controller.sh
@@ -0,0 +1,12 @@
+# i000.11.Controller.sh
+#
+# Build Controller server
+#
+#
+#
+
+# Python OpenStackClient
+bash i020.9.1.python_openstackclient-7.1.3.sh
+
+# MariaDB
+bash i020.9.2.1.MariaDB.sh
---
+++ 24.2.Dalmatian/i020.support.Major/i020.5.2.1.MariaDB.sh
@@ -0,0 +1,12 @@
+# i020.5.2.1.MariaDB.sh
+#
+# Build MariaDB from BLfS
+#
+# https://docs.openstack.org/install-guide/environment-sql-database.html
+#
+
+# MariaDB support apps
+bash c11.03.8.all.support.apps.sh
+
+# MariaDB
+bash c11.03.9.main.app.sh
