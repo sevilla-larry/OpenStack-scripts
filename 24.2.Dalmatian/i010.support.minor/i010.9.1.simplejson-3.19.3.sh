f84e3166e42057fed3be6b87b0f0bb2a1adfde64
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-12 10:58:02 +0800

    create log directory

--- 24.2.Dalmatian/i010.support.minor/i010.9.1.simplejson-3.19.3.sh
+++ 24.2.Dalmatian/i010.support.minor/i010.9.1.simplejson-3.19.3.sh
@@ -7,11 +7,16 @@ export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
 
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+
 echo "Pip3 Install ..."
 echo "Pip3 Install ..." >> $LFSLOG_PROCESS
 echo "Pip3 Install ..." >> $PKGLOG_ERROR
 
 pip3 install $PKG > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
 
 unset LFSLOG_PROCESS
 unset PKGLOG_INSTALL
