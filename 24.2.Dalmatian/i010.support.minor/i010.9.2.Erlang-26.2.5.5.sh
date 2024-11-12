e196af2be44e34b339033f32d9ae07e00737e1cb
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-12 10:48:43 +0800

    erlang update

--- 24.2.Dalmatian/i010.support.minor/i010.9.2.Erlang-26.2.5.5.sh
+++ 24.2.Dalmatian/i010.support.minor/i010.9.2.Erlang-26.2.5.5.sh
@@ -35,8 +35,7 @@ export ERL_TOP=`pwd`    # current directory
 echo "2. Configure ..."
 echo "2. Configure ..." >> $LFSLOG_PROCESS
 echo "2. Configure ..." >> $PKGLOG_ERROR
-./configure > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
-# will be install in /usr/local/bin
+./configure --prefix=/usr > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
 echo "3. Make Build ..."
 echo "3. Make Build ..." >> $LFSLOG_PROCESS
