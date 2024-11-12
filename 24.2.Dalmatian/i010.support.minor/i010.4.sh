acc1156cb0f516a96135c3790633d83f87b2d40d
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-12 15:36:29 +0800

    Elixir

--- 24.2.Dalmatian/i010.support.minor/i010.4.sh
+++ 24.2.Dalmatian/i010.support.minor/i010.4.sh
@@ -36,6 +36,20 @@ echo "." >> $LFSLOG_PROCESS
 echo "."
 echo "."
 
+###
+
+echo "i010.9.3.Elixir-1.16.3"
+echo "i010.9.3.Elixir-1.16.3" >> $LFSLOG_PROCESS
+
+time { bash i010.9.3.Elixir-1.16.3.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
 #####
 
 unset LFSLOG_PROCESS
---
+++ 24.2.Dalmatian/i010.support.minor/i010.9.3.Elixir-1.16.3.sh
@@ -0,0 +1,54 @@
+# i010.9.3.Elixir-1.16.3.sh
+#
+
+#
+# Required by:
+#
+#               RabbitMQ
+#
+#
+
+export PKG="elixir-1.16.3"
+export PKGLOG_DIR=$LFSLOG/010.3
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $LFSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. Make Build ..."
+echo "2. Make Build ..." >> $LFSLOG_PROCESS
+echo "2. Make Build ..." >> $PKGLOG_ERROR
+make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "3. Copy Install ..."
+echo "3. Copy Install ..." >> $LFSLOG_PROCESS
+echo "3. Copy Install ..." >> $PKGLOG_ERROR
+
+cp -v	bin/elixir	/usr/bin	\
+		>> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+cp -v	bin/iex		/usr/bin	\
+		>> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+
+cd ..
+rm -rf $PKG
+unset LFSLOG_PROCESS
+#unset PKGLOG_OTHERS
+unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
