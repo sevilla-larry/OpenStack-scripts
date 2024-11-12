2554742dd12ca1b3d72e9b00f6baf5dc8aded560
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-12 16:29:16 +0800

    update procedure

--- 24.2.Dalmatian/i020.support.Major/i020.4.all.support.apps.sh
+++
@@ -1,6 +0,0 @@
-# i020.4.all.support.apps.sh
-#
-# Build all Apps in i010
-#
-
-# bash i010.xxx
--- 24.2.Dalmatian/i020.support.Major/i020.5.2.1.MariaDB.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.5.2.1.MariaDB.sh
@@ -5,8 +5,5 @@
 # https://docs.openstack.org/install-guide/environment-sql-database.html
 #
 
-# MariaDB support apps
-bash c11.03.8.all.support.apps.sh
-
 # MariaDB
 bash c11.03.9.main.app.sh
