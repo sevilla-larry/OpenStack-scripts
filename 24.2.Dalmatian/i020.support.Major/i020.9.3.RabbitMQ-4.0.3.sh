9a8fbe07eecf1b0c38a7038bdc0355d119388b82
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-13 17:22:47 +0800

    rabbitmq

--- 24.2.Dalmatian/i020.support.Major/i020.9.3.RabbitMQ-4.0.3.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.3.RabbitMQ-4.0.3.sh
@@ -1,15 +1,17 @@
-# i010.9.3.Elixir-1.16.3.sh
+# i020.9.3.RabbitMQ-4.0.3.sh
 #
 
 #
-# Required by:
+# Dependencies Required:
 #
-#               RabbitMQ
-#
+#               a.08.52 Python-3.13.0		(no SQLite)
+#				i010.1	simplejson-3.19.3
+#				i010.2	Erlang-26.2.5.5
+#				i010.3	Elixir-1.16.3
 #
 
-export PKG="elixir-1.16.3"
-export PKGLOG_DIR=$LFSLOG/010.3
+export PKG="rabbitmq-server-4.0.3"
+export PKGLOG_DIR=$LFSLOG/020.3
 export PKGLOG_TAR=$PKGLOG_DIR/tar.log
 #export PKGLOG_CONFIG=$PKGLOG_DIR/config.log
 export PKGLOG_BUILD=$PKGLOG_DIR/build.log
@@ -25,7 +27,7 @@ mkdir $PKGLOG_DIR
 echo "1. Extract tar..."
 echo "1. Extract tar..." >> $LFSLOG_PROCESS
 echo "1. Extract tar..." >> $PKGLOG_ERROR
-tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+tar xvf $PKG.tar.xz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
 cd $PKG
 
 
@@ -34,16 +36,22 @@ echo "2. Make Build ..." >> $LFSLOG_PROCESS
 echo "2. Make Build ..." >> $PKGLOG_ERROR
 make > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
-echo "3. Copy Install ..."
-echo "3. Copy Install ..." >> $LFSLOG_PROCESS
-echo "3. Copy Install ..." >> $PKGLOG_ERROR
+echo "3. Make Install ..."
+echo "3. Make Install ..." >> $LFSLOG_PROCESS
+echo "3. Make Install ..." >> $PKGLOG_ERROR
+# initially install to a temporary directory
+make install DESTDIR=/tmp/rmq	\
+			> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
-mkdir /opt/elixir
-
-cp -vr {bin,lib} /opt/elixir	\
+# then move the needed directory to /opt
+mv -v /tmp/rmq/usr/local/lib/erlang/lib/rabbitmq-server-4.0.3	\
+	/opt/rabbitmq	\
 		>> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
-pathprepend /opt/elixir/bin PATH
+# and remove the temporary directory
+rm -rfv /tmp/rmq
+
+pathprepend /opt/rabbitmq/sbin PATH
 
 
 cd ..
