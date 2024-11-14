dbd2646b9313c220d6650f2b9f18d39a793ffa27
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-14 16:53:47 +0800

    sysconfdir=/etc

--- 24.2.Dalmatian/i020.support.Major/i020.9.4.1.MemCached-1.6.32.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.4.1.MemCached-1.6.32.sh
@@ -31,7 +31,9 @@ cd $PKG
 echo "2. Configure ..."
 echo "2. Configure ..." >> $LFSLOG_PROCESS
 echo "2. Configure ..." >> $PKGLOG_ERROR
-./configure --prefix=/usr > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
+./configure --prefix=/usr       \
+            --sysconfdir=/etc   \
+             > $PKGLOG_CONFIG 2>> $PKGLOG_ERROR
 
 echo "3. Make Build ..."
 echo "3. Make Build ..." >> $LFSLOG_PROCESS
