600a24f24d90ab8c74e51279f32b728432185342
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-14 17:41:39 +0800

    memcached

---
+++ 24.2.Dalmatian/i020.support.Major/i020.5.4.sh
@@ -0,0 +1,43 @@
+# i020.5.4.sh
+#
+# Build MemCached & python-MemCached
+#
+# https://docs.openstack.org/install-guide/environment-memcached.html
+#
+
+export LFSLOG_PROCESS=$LFSLOG/process.log
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+#####
+
+echo "i020.9.4.1.MemCached-1.6.32"
+echo "i020.9.4.1.MemCached-1.6.32" >> $LFSLOG_PROCESS
+
+time { bash i020.9.4.1.MemCached-1.6.32.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "i020.9.4.2.python_memcached-1.62"
+echo "i020.9.4.2.python_memcached-1.62" >> $LFSLOG_PROCESS
+
+time { bash i020.9.4.2.python_memcached-1.62.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $LFSLOG_PROCESS
+
+echo "." >> $LFSLOG_PROCESS
+echo "."
+echo "."
+
+#####
+
+unset LFSLOG_PROCESS
