3389aa259ed6f6d492df5647fd55c93a3a994b90
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-19 15:55:10 +0800

    log bug fix

--- 24.2.Dalmatian/i020.support.Major/i020.9.5.1.MemCacheD-1.6.32.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.5.1.MemCacheD-1.6.32.sh
@@ -15,7 +15,7 @@ export PKGLOG_BUILD=$PKGLOG_DIR/build.log
 export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
-#export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
+export PKGLOG_OTHERS=$PKGLOG_DIR/others.log
 export LFSLOG_PROCESS=$LFSLOG/process.log
 
 rm -r $PKGLOG_DIR 2> /dev/null
@@ -75,7 +75,7 @@ EOF
 cd ..
 rm -rf $PKG
 unset LFSLOG_PROCESS
-#unset PKGLOG_OTHERS
+unset PKGLOG_OTHERS
 unset PKGLOG_INSTALL PKGLOG_BUILD PKGLOG_CONFIG
 unset PKGLOG_CHECK
 unset PKGLOG_ERROR PKGLOG_TAR
