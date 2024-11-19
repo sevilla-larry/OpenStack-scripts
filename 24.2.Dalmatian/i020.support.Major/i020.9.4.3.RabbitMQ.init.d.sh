cff1d351fdf0ee04a847e7d139b9e4bb9ed2b923
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-19 15:04:08 +0800

    init.d script update

--- 24.2.Dalmatian/i020.support.Major/i020.9.4.3.RabbitMQ.init.d.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.4.3.RabbitMQ.init.d.sh
@@ -31,7 +31,8 @@ case "$1" in
    start)
       log_info_msg "Starting RabbitMQ Server..."
 
-      start_daemon /usr/local/rabbitmq_server-4.0.3/sbin/rabbitmq-server -detached
+      #start_daemon /usr/local/rabbitmq_server-4.0.3/sbin/rabbitmq-server -detached
+      /usr/local/rabbitmq_server-4.0.3/sbin/rabbitmq-server -detached
       evaluate_retval
       ;;
 
