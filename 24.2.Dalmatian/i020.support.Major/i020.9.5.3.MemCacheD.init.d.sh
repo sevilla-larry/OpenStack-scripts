fa56c56246d1fb5cf983d4310d20e8b0d2c9b9f6
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2024-11-19 20:13:25 +0800

    bug fix

--- 24.2.Dalmatian/i020.support.Major/i020.9.5.3.MemCacheD.init.d.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.5.3.MemCacheD.init.d.sh
@@ -34,7 +34,7 @@ case "$1" in
       install -dm755 -o memcached -g memcached /run/memcached
 
       if ! pidofproc memcached >/dev/null; then
-          su - memcached -c '/usr/bin/memcached'
+          su - memcached -c '/usr/bin/memcached -d'
           #or# /usr/bin/memcached -d -u memcached
       fi
 
