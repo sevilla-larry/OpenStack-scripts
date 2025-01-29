bc969ecc6bf6b4f36d097de0ef4c837206b8694e
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-01-29 18:18:36 +0800

    update mod-wsgi & python-memcached

--- 24.2.Dalmatian/i020.support.Major/i020.9.3.2.mod-wsgi-5.0.2.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.3.2.mod-wsgi-5.0.2.sh
@@ -1,61 +1,55 @@
 # i020.9.3.2.mod-wsgi-5.0.2.sh
 #
-# https://docs.openstack.org/keystone/2024.2/install/
-# https://docs.openstack.org/keystone/2024.2/install/keystone-install-ubuntu.html
-#
-# will download Python App online
-#
 
-export PKG="mod-wsgi==5.0.2"
+export PKG="mod-wsgi-5.0.2"
 export PKGLOG_DIR=$OSLOG/020.3
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 export OSLOG_PROCESS=$OSLOG/process.log
 
-# export APACHE_DATA_DIR=/srv/www
-# export APACHE_CONF_DIR=/etc/httpd/
-# export APACHE_CONF_FILE=$APACHE_CONF_DIR/httpd.conf
-# export APACHE_CONF_EXTRA_DIR=$APACHE_CONF_DIR/extra
-# export WSGI_TEST_PYTH_SRC=$PWD/i020.9.3.3.wsgi-test.py
-# export WSGI_TEST_PYTH_DST=$APACHE_DATA_DIR/wsgi-test.py
-# export WSGI_TEST_CONF_SRC=$PWD/i020.9.3.4.wsgi-test.conf
-# export WSGI_TEST_CONF_DST=$APACHE_CONF_EXTRA_DIR/wsgi-test.conf
-
 rm -r $PKGLOG_DIR 2> /dev/null
 mkdir $PKGLOG_DIR
 
-
-echo "Pip3 Install ..."
-echo "Pip3 Install ..." >> $OSLOG_PROCESS
-echo "Pip3 Install ..." >> $PKGLOG_ERROR
-
-pip3 install $PKG > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
-
-# cp -v   $WSGI_TEST_PYTH_SRC $WSGI_TEST_PYTH_DST \
-#         >> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
-# cp -v   $WSGI_TEST_CONF_SRC $WSGI_TEST_CONF_DST \
-#         >> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
-# chown -v apache:apache $WSGI_TEST_PYTH_DST $WSGI_TEST_CONF_DST  \
-#         >> $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
-
-# cat >> $APACHE_CONF_FILE << "EOF"  2>> $PKGLOG_ERROR
-
-# # add wsgi test configuration file
-# Include /etc/httpd/extra/wsgi-test.conf
-# EOF
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
 
 
-# unset WSGI_TEST_PYTH_SRC
-# unset WSGI_TEST_PYTH_DST
-# unset WSGI_TEST_CONF_SRC
-# unset WSGI_TEST_CONF_DST
+echo "2. pip3 Build ..."
+echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $PKGLOG_ERROR
+pip3 wheel  -w dist                 \
+            --no-cache-dir          \
+            --no-build-isolation    \
+            --no-deps               \
+            $PWD                    \
+            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
 
-# unset APACHE_DATA_DIR
-# unset APACHE_CONF_DIR
-# unset APACHE_CONF_FILE
-# unset APACHE_CONF_EXTRA_DIR
+echo "3. pip3 Install ..."
+echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $PKGLOG_ERROR
+pip3 install    --no-index              \
+                --no-user               \
+                --find-links dist       \
+                --no-cache-dir          \
+                mod-wsgi                \
+                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
+# echo "4. pyTest ..."
+# echo "4. pyTest ..." >> $OSLOG_PROCESS
+# echo "4. pyTest ..." >> $PKGLOG_ERROR
+# pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+
+cd ..
+rm -rf $PKG
 unset OSLOG_PROCESS
-unset PKGLOG_INSTALL
-unset PKGLOG_ERROR
+unset PKGLOG_INSTALL PKGLOG_BUILD
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
 unset PKGLOG_DIR PKG
--- 24.2.Dalmatian/i020.support.Major/i020.9.5.2.python_memcached-1.62.sh
+++ 24.2.Dalmatian/i020.support.Major/i020.9.5.2.python_memcached-1.62.sh
@@ -1,10 +1,11 @@
 # i020.9.5.2.python_memcached-1.62.sh
 #
-# https://docs.openstack.org/install-guide/environment-memcached.html
-#
 
-export PKG="python_memcached-1.62-py2.py3-none-any.whl"
+export PKG="python_memcached-1.62"
 export PKGLOG_DIR=$OSLOG/020.5.2
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 export OSLOG_PROCESS=$OSLOG/process.log
@@ -12,15 +13,43 @@ export OSLOG_PROCESS=$OSLOG/process.log
 rm -r $PKGLOG_DIR 2> /dev/null
 mkdir $PKGLOG_DIR
 
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
 
-echo "Pip3 Install ..."
-echo "Pip3 Install ..." >> $OSLOG_PROCESS
-echo "Pip3 Install ..." >> $PKGLOG_ERROR
 
-pip3 install $PKG > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+echo "2. pip3 Build ..."
+echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $PKGLOG_ERROR
+pip3 wheel  -w dist                 \
+            --no-cache-dir          \
+            --no-build-isolation    \
+            --no-deps               \
+            $PWD                    \
+            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "3. pip3 Install ..."
+echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $PKGLOG_ERROR
+pip3 install    --no-index              \
+                --no-user               \
+                --find-links dist       \
+                --no-cache-dir          \
+                python_memcached        \
+                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+# echo "4. pyTest ..."
+# echo "4. pyTest ..." >> $OSLOG_PROCESS
+# echo "4. pyTest ..." >> $PKGLOG_ERROR
+# pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
 
 
+cd ..
+rm -rf $PKG
 unset OSLOG_PROCESS
-unset PKGLOG_INSTALL
-unset PKGLOG_ERROR
+unset PKGLOG_INSTALL PKGLOG_BUILD
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
 unset PKGLOG_DIR PKG
