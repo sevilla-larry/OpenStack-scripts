77ea4797e44b42193149518056e523e120089ea4
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-02-02 10:38:09 +0800

    bcrypt change install due to err

--- 24.2.Dalmatian/i030.Oslo.support/i030.9.13.bcrypt-4.1.3.sh
+++ 24.2.Dalmatian/i030.Oslo.support/i030.9.13.bcrypt-4.1.3.sh
@@ -1,5 +1,8 @@
 # i030.9.13.bcrypt-4.1.3.sh
 #
+# Note: (alternative) .whl install
+# since the source build is giving ERRORs
+#
 
 #
 # Required by:
@@ -8,11 +11,8 @@
 #               i151.8    KeyStone-26.0.0
 #
 
-export PKG="bcrypt-4.1.3"
-export PKGLOG_DIR=$OSLOG/030.13
-export PKGLOG_TAR=$PKGLOG_DIR/tar.log
-export PKGLOG_BUILD=$PKGLOG_DIR/build.log
-export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKG="bcrypt-4.1.3-cp39-abi3-manylinux_2_28_x86_64.whl"
+export PKGLOG_DIR=$OSLOG/050.18
 export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
 export PKGLOG_ERROR=$PKGLOG_DIR/error.log
 export OSLOG_PROCESS=$OSLOG/process.log
@@ -20,43 +20,15 @@ export OSLOG_PROCESS=$OSLOG/process.log
 rm -r $PKGLOG_DIR 2> /dev/null
 mkdir $PKGLOG_DIR
 
-echo "1. Extract tar..."
-echo "1. Extract tar..." >> $OSLOG_PROCESS
-echo "1. Extract tar..." >> $PKGLOG_ERROR
-tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
-cd $PKG
+
+echo "Pip3 Install ..."
+echo "Pip3 Install ..." >> $OSLOG_PROCESS
+echo "Pip3 Install ..." >> $PKGLOG_ERROR
+
+pip3 install $PKG > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 
-echo "2. pip3 Build ..."
-echo "2. pip3 Build ..." >> $OSLOG_PROCESS
-echo "2. pip3 Build ..." >> $PKGLOG_ERROR
-pip3 wheel  -w dist                 \
-            --no-cache-dir          \
-            --no-build-isolation    \
-            --no-deps               \
-            $PWD                    \
-            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
-
-echo "3. pip3 Install ..."
-echo "3. pip3 Install ..." >> $OSLOG_PROCESS
-echo "3. pip3 Install ..." >> $PKGLOG_ERROR
-pip3 install    --no-index              \
-                --no-user               \
-                --find-links dist       \
-                --no-cache-dir          \
-                bcrypt                  \
-                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
-
-# echo "4. pyTest ..."
-# echo "4. pyTest ..." >> $OSLOG_PROCESS
-# echo "4. pyTest ..." >> $PKGLOG_ERROR
-# pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
-
-
-cd ..
-rm -rf $PKG
 unset OSLOG_PROCESS
-unset PKGLOG_INSTALL PKGLOG_BUILD
-unset PKGLOG_CHECK
-unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_INSTALL
+unset PKGLOG_ERROR
 unset PKGLOG_DIR PKG
