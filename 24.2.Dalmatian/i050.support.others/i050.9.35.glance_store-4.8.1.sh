60e5ca29712c9044dffd7aa07e00c961651c0125
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-02-03 13:03:30 +0800

    fix glance-store

--- 24.2.Dalmatian/i050.support.others/i050.9.35.glance_store-4.8.1.sh
+++ 24.2.Dalmatian/i050.support.others/i050.9.35.glance_store-4.8.1.sh
@@ -59,7 +59,7 @@ pip3 install    --no-index              \
                 --no-user               \
                 --find-links dist       \
                 --no-cache-dir          \
-                glance_store            \
+                glance-store            \
                 > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
 
 # echo "4. pyTest ..."
