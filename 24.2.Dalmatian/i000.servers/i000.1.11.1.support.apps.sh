c4ffb9992d6ec53677957343931bfc9a6c1379f7
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-03-24 06:57:30 +0800

    OS renumbering 9

--- 24.2.Dalmatian/i000.servers/i000.1.11.1.support.apps.sh
+++ 24.2.Dalmatian/i000.servers/i000.1.11.1.support.apps.sh
@@ -36,9 +36,14 @@ bash b30.03.2.sh
 
 
 # Python OpenStackClient support
-bash i010.5.11.1.sh
+bash i010.5.11.sh   # Oslo and OpenStack support apps   - 1
+bash i020.5.11.sh   # OpenStack sub-Services apps       - 1
+bash i025.5.11.sh   # Oslo and OpenStack support apps   - 2
+bash i030.5.11.sh   # Oslo apps                         - 1
+bash i040.5.11.sh   # OpenStack sub-Services apps       - 2
+
+# support apps minor for Controller
+bash i045.5.11.sh
+
 # # Python OpenStackClient
 # bash i020.5.1.sh
-
-# support apps for Controller
-bash i010.5.11.2.sh
--- 24.2.Dalmatian/i045.support.minor/i045.5.11.sh
+++ 24.2.Dalmatian/i045.support.minor/i045.5.11.sh
@@ -1,6 +1,6 @@
-# i005.5.11.2.sh
+# i045.5.11.2.sh
 #
-# Build all Apps in i005
+# Build all Apps in i045
 #
 
 export OSLOG_PROCESS=$OSLOG/process.log
@@ -13,10 +13,10 @@ date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
 #
 # RabbitMQ is Binary Installed, simplejson is for Source building
 
-# echo "i010.9.1.simplejson-3.19.3"
-# echo "i010.9.1.simplejson-3.19.3" >> $OSLOG_PROCESS
+# echo "i045.9.1.simplejson-3.19.3"
+# echo "i045.9.1.simplejson-3.19.3" >> $OSLOG_PROCESS
 
-# time { bash i010.9.1.simplejson-3.19.3.sh ; }
+# time { bash i045.9.1.simplejson-3.19.3.sh ; }
 
 # date +"%Y/%b/%d %H:%M"
 # date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
@@ -27,10 +27,10 @@ date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
 
 ###
 
-echo "i010.9.92.Erlang-26.2.5.5"
-echo "i010.9.92.Erlang-26.2.5.5" >> $OSLOG_PROCESS
+echo "i045.9.92.Erlang-26.2.5.5"
+echo "i045.9.92.Erlang-26.2.5.5" >> $OSLOG_PROCESS
 
-time { bash i010.9.92.Erlang-26.2.5.5.sh ; }
+time { bash i045.9.92.Erlang-26.2.5.5.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
@@ -43,10 +43,10 @@ echo "."
 #
 # RabbitMQ is Binary Installed, Exlixir is for Source building
 
-# echo "i010.9.3.Elixir-1.16.3"
-# echo "i010.9.3.Elixir-1.16.3" >> $OSLOG_PROCESS
+# echo "i045.9.3.Elixir-1.16.3"
+# echo "i045.9.3.Elixir-1.16.3" >> $OSLOG_PROCESS
 
-# time { bash i010.9.3.Elixir-1.16.3.sh ; }
+# time { bash i045.9.3.Elixir-1.16.3.sh ; }
 
 # date +"%Y/%b/%d %H:%M"
 # date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
@@ -57,10 +57,10 @@ echo "."
 
 ###
 
-# echo "i010.9.94.build-1.2.1"
-# echo "i010.9.94.build-1.2.1" >> $OSLOG_PROCESS
+# echo "i045.9.94.build-1.2.1"
+# echo "i045.9.94.build-1.2.1" >> $OSLOG_PROCESS
 
-# time { bash i010.9.94.build-1.2.1.sh ; }
+# time { bash i045.9.94.build-1.2.1.sh ; }
 
 # date +"%Y/%b/%d %H:%M"
 # date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
@@ -73,10 +73,10 @@ echo "."
 #       Note: Already included in LfS 12.2
 # ###
 
-# echo "i010.9.5.sshpass-1.10"
-# echo "i010.9.5.sshpass-1.10" >> $OSLOG_PROCESS
+# echo "i045.9.5.sshpass-1.10"
+# echo "i045.9.5.sshpass-1.10" >> $OSLOG_PROCESS
 
-# time { bash i010.9.5.sshpass-1.10.sh ; }
+# time { bash i045.9.5.sshpass-1.10.sh ; }
 
 # date +"%Y/%b/%d %H:%M"
 # date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
