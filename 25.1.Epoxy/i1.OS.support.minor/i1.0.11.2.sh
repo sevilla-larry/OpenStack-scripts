fb240486dfd0678554ca2b1907d2e698f95396b7
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-10-10 21:15:23 +0800

    add test* & deps

--- 25.1.Epoxy/i1.OS.support.minor/i1.0.11.2.sh
+++ 25.1.Epoxy/i1.OS.support.minor/i1.0.11.2.sh
@@ -28,6 +28,59 @@ echo "."
 
 ###
 
+echo "i1.2.02.testresources-2.0.2"
+echo "i1.2.02.testresources-2.0.2" >> $OSLOG_PROCESS
+
+time { bash i1.2.02.testresources-2.0.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
+
+echo "." >> $OSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "i1.2.03.fixtures-4.1.0"
+echo "i1.2.03.fixtures-4.1.0" >> $OSLOG_PROCESS
+
+time { bash i1.2.03.fixtures-4.1.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
+
+echo "." >> $OSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "i1.2.04.testtools-2.7.2"
+echo "i1.2.04.testtools-2.7.2" >> $OSLOG_PROCESS
+
+time { bash i1.2.04.testtools-2.7.2.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
+
+echo "." >> $OSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "i1.2.05.testscenarios-0.5.0"
+echo "i1.2.05.testscenarios-0.5.0" >> $OSLOG_PROCESS
+
+time { bash i1.2.05.testscenarios-0.5.0.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
+
+echo "." >> $OSLOG_PROCESS
+echo "."
+echo "."
 
 #####
 
---
+++ 25.1.Epoxy/i1.OS.support.minor/i1.2.05.testscenarios-0.5.0.sh
@@ -0,0 +1,70 @@
+# i1.2.05.testscenarios-0.5.0.sh
+#
+
+#
+# Dependencies Required:
+#
+#               i3.1.01 pbr-6.1.1
+#               i1.2.04 testtools-2.7.2
+#
+
+#
+# Required by:
+#
+#               i4.2.35 oslo.db-17.2.1
+#
+
+export PKG="testscenarios-0.5.0"
+export PKGLOG_DIR=$OSLOG/1.2.05
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export OSLOG_PROCESS=$OSLOG/process.log
+export SOURCES=`pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. pip3 Build ..."
+echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $PKGLOG_ERROR
+pip3 wheel  -w dist                 \
+            --no-cache-dir          \
+            --no-build-isolation    \
+            --no-deps               \
+            $PWD                    \
+            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "3. pip3 Install ..."
+echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $PKGLOG_ERROR
+pip3 install    --no-index              \
+                --no-user               \
+                --find-links dist       \
+                --no-cache-dir          \
+                testscenarios           \
+                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+echo "4. pyTest ..."
+echo "4. pyTest ..." >> $OSLOG_PROCESS
+echo "4. pyTest ..." >> $PKGLOG_ERROR
+pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset OSLOG_PROCESS
+unset PKGLOG_INSTALL PKGLOG_BUILD
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
