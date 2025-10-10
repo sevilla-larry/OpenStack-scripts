51879ff11c41b99dd61b5237ec946263a682d74f
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-10-10 13:55:06 +0800

    add dnspython & eventlet

---
+++ 25.1.Epoxy/i1.OS.support.minor/i1.1.35.dnspython-2.7.0.sh
@@ -0,0 +1,63 @@
+# i1.1.35.dnspython-2.7.0.sh
+#
+
+#
+# Required by:
+#
+#               i1.1.36 eventlet-0.39.1
+#
+
+export PKG="dnspython-2.7.0"
+export PKGLOG_DIR=$OSLOG/1.1.35
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export OSLOG_PROCESS=$OSLOG/process.log
+export SOURCES=`pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. pip3 Build ..."
+echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $PKGLOG_ERROR
+pip3 wheel  -w dist                 \
+            --no-cache-dir          \
+            --no-build-isolation    \
+            --no-deps               \
+            $PWD                    \
+            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "3. pip3 Install ..."
+echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $PKGLOG_ERROR
+pip3 install    --no-index              \
+                --no-user               \
+                --find-links dist       \
+                --no-cache-dir          \
+                dnspython               \
+                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+echo "4. pyTest ..."
+echo "4. pyTest ..." >> $OSLOG_PROCESS
+echo "4. pyTest ..." >> $PKGLOG_ERROR
+pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset OSLOG_PROCESS
+unset PKGLOG_INSTALL PKGLOG_BUILD
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
