4669b6e7d373d6d2937e34d50b71404d6a5471b7
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-10-11 14:53:32 +0800

    add osprofiler

--- 25.1.Epoxy/i3.OS.Hosted.apps/i3.0.11.2.sh
+++ 25.1.Epoxy/i3.OS.Hosted.apps/i3.0.11.2.sh
@@ -12,12 +12,30 @@ export OSLOG_PROCESS=$OSLOG/process.log
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
 
+## these apps
+## depends on
+## oslo.* apps
+
 #####
 
 echo "i3.2.01.pycadf-4.0.1"
 echo "i3.2.01.pycadf-4.0.1" >> $OSLOG_PROCESS
 
 time { bash i3.2.01.pycadf-4.0.1.sh ; }
+
+date +"%Y/%b/%d %H:%M"
+date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
+
+echo "." >> $OSLOG_PROCESS
+echo "."
+echo "."
+
+###
+
+echo "i3.2.02.osprofiler-4.2.0"
+echo "i3.2.02.osprofiler-4.2.0" >> $OSLOG_PROCESS
+
+time { bash i3.2.02.osprofiler-4.2.0.sh ; }
 
 date +"%Y/%b/%d %H:%M"
 date +"%Y/%b/%d %H:%M" >> $OSLOG_PROCESS
