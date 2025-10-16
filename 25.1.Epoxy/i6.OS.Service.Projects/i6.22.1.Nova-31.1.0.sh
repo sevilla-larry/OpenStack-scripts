1ff9070102811f8774c9796818e364d915872db6
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-10-16 09:32:51 +0800

    update Nova

--- 25.1.Epoxy/i6.OS.Service.Projects/i6.22.1.Nova-31.1.0.sh
+++ 25.1.Epoxy/i6.OS.Service.Projects/i6.22.1.Nova-31.1.0.sh
@@ -6,67 +6,67 @@
 #
 # Dependencies Required:
 #
-#               i3.1.01 pbr-6.1.1
-#               i1.1.30 SQLAlchemy-2.0.43
-decorator>=4.1.0
-eventlet>=0.30.1
-Jinja2>=2.10
-keystonemiddleware>=4.20.0
-lxml>=4.5.0
-Routes>=2.3.1
-cryptography>=2.7
-WebOb>=1.8.2
-greenlet>=0.4.15
-PasteDeploy>=1.5.0
-Paste>=2.0.2
-PrettyTable>=0.7.1
-alembic>=1.5.0
-netaddr>=0.7.18
-paramiko>=2.7.1
-iso8601>=0.1.11
-jsonschema>=4.0.0
-python-cinderclient>=4.0.1
-keystoneauth1>=3.16.0
-python-neutronclient>=7.1.0
-python-glanceclient>=2.8.0
-requests>=2.25.1
-stevedore>=1.20.0
-websockify>=0.9.0
-oslo.cache>=1.26.0
-oslo.concurrency>=5.0.1
-oslo.config>=8.6.0
-oslo.context>=3.4.0
-oslo.log>=4.6.1
-oslo.limit>=1.5.0
-oslo.reports>=1.18.0
-oslo.serialization>=4.2.0
-oslo.upgradecheck>=1.3.0
-oslo.utils>=8.0.0
-oslo.db>=10.0.0
-oslo.rootwrap>=5.15.0
-oslo.messaging>=14.1.0
-oslo.policy>=4.5.0
-oslo.privsep>=2.6.2
-oslo.i18n>=5.1.0
-oslo.service>=2.8.0
-rfc3986>=1.2.0
-oslo.middleware>=3.31.0
-psutil>=3.2.2
-oslo.versionedobjects>=1.35.0
-os-brick>=6.10.0
-os-resource-classes>=1.1.0
-os-traits>=3.3.0
-os-vif>=3.1.0
-castellan>=0.16.0
-microversion-parse>=0.2.1
-tooz>=1.58.0
-cursive>=0.2.1
-retrying>=1.3.3
-os-service-types>=1.7.0
-python-dateutil>=2.7.0
-futurist>=1.8.0
-openstacksdk>=4.4.0
-PyYAML>=5.1
+#               i3.1.01      pbr-6.1.1
+#               i1.1.30      SQLAlchemy-2.0.43
+#               i1.1.10      decorator-5.2.1
+#               i1.1.36      eventlet-0.39.1
+#               a.08.75      Jinja2-3.1.6
+#               i4.3.07      keystonemiddleware-10.9.0
+#               b10.13.24.13 lxml-6.0.0
+#               i1.1.40      Routes-2.5.1
+#               i1.1.03      cryptography-43.0.3 whl
+#               i1.1.21      WebOb-1.8.9
+#               i1.1.29      greenlet-3.2.4
+#               i1.1.38      PasteDeploy-3.1.0
+#               i1.1.41      Paste-3.10.1
+#               i1.1.08      prettytable-3.15.1
+#               i1.1.49      alembic-1.14.1
+#               i1.1.16      netaddr-1.3.0
+#paramiko>=2.7.1
+#               i1.1.09      iso8601-2.1.0.whl
+#               i1.1.59      jsonschema-4.23.0
+#               i5.1.07      python-cinderclient-9.7.0
+#               i4.1.06      keystoneauth1-5.10.0
+#python-neutronclient>=7.1.0
+#               i5.1.12      python-glanceclient-4.8.0
+#               b30.13.24.30 Requests-2.32.5
+#               i4.1.20      stevedore-5.4.1
+#websockify>=0.9.0
+#               i4.2.31      oslo.cache-3.10.2
+#               i4.1.32      oslo.concurrency-7.1.0
+#               i4.1.33      oslo.config-9.7.1
+#               i4.1.34      oslo.context-5.7.1
+#               i4.1.38      oslo.log-7.1.0
+#               i4.2.37      oslo.limit-2.6.1
+#               i4.1.44      oslo.reports-3.5.1
+#               i4.1.46      oslo.serialization-5.7.0
+#               i4.1.48      oslo.upgradecheck-2.5.0
+#               i4.1.49      oslo.utils-8.2.0
+#               i4.2.35      oslo.db-17.2.1
+#oslo.rootwrap>=5.15.0
+#               i4.1.39      oslo.messaging-16.1.0
+#               i4.1.42      oslo.policy-4.5.1
+#oslo.privsep>=2.6.2
+#               i4.1.36      oslo.i18n-6.5.1
+#               i4.1.47      oslo.service-4.1.1
+#               i1.1.19      rfc3986-2.0.0
+#               i4.1.41      oslo.middleware-6.3.1
+#               b10.13.24.17 psutil-7.0.0
+#oslo.versionedobjects>=1.35.0
+#os-brick>=6.10.0
+#               i3.1.07      os-resource-classes-1.1.0
+#               i3.1.08      os-traits-3.4.0
+#os-vif>=3.1.0
+#               i4.2.01      castellan-5.2.1
+#               i3.1.09      microversion-parse-2.0.0
+#tooz>=1.58.0
+#               i1.3.01      cursive-0.2.3
+#               i1.1.69      retrying-1.3.4
+#               i3.1.02      os-service-types-1.7.0
+#               i1.1.31      python-dateutil-2.9.0.post0
+#               i3.1.05      futurist-3.0.0
+#               i4.2.14      openstacksdk-4.4.0
+#               b10.13.24.28 PyYAML-6.0.2
 
 export PKG="nova-30.0.0"
 export PKGLOG_DIR=$OSLOG/111.1
