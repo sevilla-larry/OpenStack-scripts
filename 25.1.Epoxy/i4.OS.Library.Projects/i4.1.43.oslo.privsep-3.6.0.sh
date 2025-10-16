ed88a073d75932ebc0f8fcea0a5bce876f0cbc6e
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-10-16 16:10:58 +0800

    add oslo.privsep

---
+++ 25.1.Epoxy/i4.OS.Library.Projects/i4.1.43.oslo.privsep-3.6.0.sh
@@ -0,0 +1,78 @@
+# i4.1.43.oslo.privsep-3.6.0.sh
+#
+
+#
+# Dependencies Required:
+#
+#               i030.9.06 oslo.log-6.1.2
+#               i030.9.01 oslo.i18n-6.4.0
+#               i030.9.03 oslo.config-9.6.0
+#               i030.9.02 oslo.utils-7.3.0
+#               i010.9.02 cffi-1.16.0
+#               i060.9.05 eventlet-0.36.1
+#               i060.9.04 greenlet-3.0.3
+#               b30.13.24.17 Msgpack-1.0.8
+#
+
+#
+# Required by:
+#
+#               i090.9.11 os_brick-6.9.1
+#               i090.9.13 os_vif-3.7.0
+#               i111.9.3  Nova-30.0.0
+#
+
+export PKG="oslo_privsep-3.6.0"
+export PKGLOG_DIR=$OSLOG/4.1.43
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export OSLOG_PROCESS=$OSLOG/process.log
+export SOURCES=`pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. pip3 Build ..."
+echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $PKGLOG_ERROR
+pip3 wheel  -w dist                 \
+            --no-cache-dir          \
+            --no-build-isolation    \
+            --no-deps               \
+            $PWD                    \
+            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "3. pip3 Install ..."
+echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $PKGLOG_ERROR
+pip3 install    --no-index              \
+                --no-user               \
+                --find-links dist       \
+                --no-cache-dir          \
+                oslo.privsep            \
+                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+# echo "4. pyTest ..."
+# echo "4. pyTest ..." >> $OSLOG_PROCESS
+# echo "4. pyTest ..." >> $PKGLOG_ERROR
+# pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset OSLOG_PROCESS
+unset PKGLOG_INSTALL PKGLOG_BUILD
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
