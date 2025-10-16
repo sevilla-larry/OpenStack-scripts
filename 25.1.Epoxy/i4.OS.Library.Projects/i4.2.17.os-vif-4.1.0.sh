c7ebf7000403a50527874f9d22cab814196162c1
Sevilla, Larry <sevilla.larry.prg@gmail.com>
2025-10-16 19:07:11 +0800

    add os-vif

---
+++ 25.1.Epoxy/i4.OS.Library.Projects/i4.2.17.os-vif-4.1.0.sh
@@ -0,0 +1,81 @@
+# i4.2.17.os-vif-4.1.0.sh
+#
+
+#
+# Dependencies Required:
+#
+#               i020.9.01 pbr-6.1.0
+#               i080.9.02 oslo.concurrency-6.1.0
+#               i030.9.03 oslo.config-9.6.0
+#               i030.9.06 oslo.log-6.1.2
+#               i030.9.01 oslo.i18n-6.4.0
+#               i080.9.13 oslo.privsep-3.4.0
+#               i030.9.04 oslo.serialization-5.5.0
+#               i030.9.02 oslo.utils-7.3.0
+#               i080.9.14 oslo.versionedobjects-3.4.0
+#               i090.9.12 ovsdbapp-2.8.0
+#               i060.9.31 pyroute2-0.7.12
+#               i020.9.02 stevedore-5.3.0
+#               i020.9.06 debtcollector-3.0.0
+#
+
+#
+# Required by:
+#
+#               i111.9.3  Nova-30.0.0
+#
+
+export PKG="os_vif-4.1.0"
+export PKGLOG_DIR=$OSLOG/4.2.17
+export PKGLOG_TAR=$PKGLOG_DIR/tar.log
+export PKGLOG_BUILD=$PKGLOG_DIR/build.log
+export PKGLOG_CHECK=$PKGLOG_DIR/check.log
+export PKGLOG_INSTALL=$PKGLOG_DIR/install.log
+export PKGLOG_ERROR=$PKGLOG_DIR/error.log
+export OSLOG_PROCESS=$OSLOG/process.log
+export SOURCES=`pwd`
+
+rm -r $PKGLOG_DIR 2> /dev/null
+mkdir $PKGLOG_DIR
+
+echo "1. Extract tar..."
+echo "1. Extract tar..." >> $OSLOG_PROCESS
+echo "1. Extract tar..." >> $PKGLOG_ERROR
+tar xvf $PKG.tar.gz > $PKGLOG_TAR 2>> $PKGLOG_ERROR
+cd $PKG
+
+
+echo "2. pip3 Build ..."
+echo "2. pip3 Build ..." >> $OSLOG_PROCESS
+echo "2. pip3 Build ..." >> $PKGLOG_ERROR
+pip3 wheel  -w dist                 \
+            --no-cache-dir          \
+            --no-build-isolation    \
+            --no-deps               \
+            $PWD                    \
+            > $PKGLOG_BUILD 2>> $PKGLOG_ERROR
+
+echo "3. pip3 Install ..."
+echo "3. pip3 Install ..." >> $OSLOG_PROCESS
+echo "3. pip3 Install ..." >> $PKGLOG_ERROR
+pip3 install    --no-index              \
+                --no-user               \
+                --find-links dist       \
+                --no-cache-dir          \
+                os-vif                  \
+                > $PKGLOG_INSTALL 2>> $PKGLOG_ERROR
+
+echo "4. pyTest ..."
+echo "4. pyTest ..." >> $OSLOG_PROCESS
+echo "4. pyTest ..." >> $PKGLOG_ERROR
+pytest >  $PKGLOG_CHECK 2>> $PKGLOG_ERROR
+
+
+cd $SOURCES
+rm -rf $PKG
+unset SOURCES
+unset OSLOG_PROCESS
+unset PKGLOG_INSTALL PKGLOG_BUILD
+unset PKGLOG_CHECK
+unset PKGLOG_ERROR PKGLOG_TAR
+unset PKGLOG_DIR PKG
